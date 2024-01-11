c4b93c31f1a609a231df25c09e6fbc042081ebc9
khaledha <khaledhadjali1@gmail.com>
2024-01-11 15:09:40 +0100

    pytest middlewares

---
+++ fastapi_multi_custom_middleware/tests/tets_middleware.py
@@ -0,0 +1,17 @@
+# import level
+import time
+from fastapi.testclient import TestClient
+
+# at the end of the file
+client = TestClient(app)
+
+def test_modify_request_response_middleware():
+    # Send a GET request to the hello endpoint
+    response = client.get("/norma/with_extra_heasers")
+    # Assert the response status code is 200
+    assert response.status_code == 200
+    # Assert the middleware has been applied
+    assert response.headers.get("X-Custom-Header") == "Modified"
+    # Assert the response content
+    assert response.json() == {"message": "Hello, World!"}
+    
